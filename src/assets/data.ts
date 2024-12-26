4b46e3506c9219ac60b16f76aec05e246ad3975d
Eliezer A Meza <150059983+EliezerMezaDev@users.noreply.github.com>
2024-12-26 11:33:43 -0400

    add: projects section

--- src/assets/data.ts
+++ src/assets/data.ts
@@ -2,13 +2,37 @@ const projects: EMZ.Project[] = [
   {
     id: 0,
     portraid: "images/projects/project0.webp",
-    title: "Projecto de prueba",
+    title: "Projecto de prueba 1",
     desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat labore odit iure officiis id quas.",
-    stack: ["react", "tailwind"],
+    stack: ["react", "astro"],
     images: [],
 
     preview: "https://asd.com",
     code: "https://asd.com",
+  },
+
+  {
+    id: 1,
+    portraid: "images/projects/project0.webp",
+    title: "Projecto de prueba 2",
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat labore odit iure officiis id quas.",
+    stack: ["next", "sass"],
+    images: [],
+
+    preview: "https://asd.com",
+    code: "",
+  },
+
+  {
+    id: 2,
+    portraid: "images/projects/project0.webp",
+    title: "Projecto de prueba 3",
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat labore odit iure officiis id quas.",
+    stack: ["css", "tailwind", 'tauri', "bun"],
+    images: [],
+
+    preview: "https://asd.com",
+    code: "",
   },
 ];
 
@@ -16,13 +40,13 @@ export const techs: EMZ.Tech[] = [
   {
     id: "react",
     label: "React",
-    color: "#1698c8",
+    color: "#00d8ff",
     icon: "icons/techs/react.svg",
   },
   {
     id: "astro",
     label: "Astro",
-    color: "#e741b1",
+    color: "#ff5d01",
     icon: "icons/techs/astro.svg",
   },
   {
@@ -34,31 +58,31 @@ export const techs: EMZ.Tech[] = [
   {
     id: "sass",
     label: "SASS",
-    color: "#ce6b9c",
+    color: "#b65a88",
     icon: "icons/techs/sass.svg",
   },
   {
     id: "tailwind",
     label: "Tailwind",
-    color: "#1dc0cd",
+    color: "#1ea3bb",
     icon: "icons/techs/tailwind.svg",
   },
   {
     id: "css",
     label: "CSS",
-    color: "#316af1",
+    color: "#442265",
     icon: "icons/techs/css.svg",
   },
   {
     id: "tauri",
     label: "Tauri",
-    color: "#ffc337",
+    color: "#ccaf26",
     icon: "icons/techs/tauri.svg",
   },
   {
     id: "bun",
     label: "Bun",
-    color: "#fbf0e0",
+    color: "#948976",
     icon: "icons/techs/bun.svg",
   },
 ];
@@ -67,21 +91,23 @@ const getTechByNames = (_techs: EMZ.TechID[]) => {
   return techs.filter((t) => _techs.includes(t.id));
 };
 
-export const getProjectSourceByID = (_id: number) => {
-  const p = projects.filter((p) => p.id === _id)[0];
-
-  return { ...p, tech: getTechByNames(p.stack) };
-};
-
-export const getProjectSourceByTech = (_techs: EMZ.TechID[]) => {
+export const getProjectSourceByID = (_ids: number[]) => {
   return projects
-    .filter((p) => checkFilterMatch(p.stack, _techs))
+    .filter((p) => checkFilterMatch(_ids, [p.id]))
     .map((p) => {
       return { ...p, tech: getTechByNames(p.stack) };
     });
 };
 
-const checkFilterMatch = (_source: EMZ.TechID[], _search: EMZ.TechID[]) => {
+export const getProjectSourceByTech = (_techs: EMZ.TechID[]) => {
+  return projects
+    .filter((p) => checkFilterMatch(p.stack, _techs) || _techs.length === 0)
+    .map((p) => {
+      return { ...p, tech: getTechByNames(p.stack) };
+    });
+};
+
+const checkFilterMatch = (_source: any[], _search: any[]) => {
   const matches = _search.filter((s) => _source.includes(s));
 
   return matches.length === _search.length;
